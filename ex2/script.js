de52c14eb3e79d778be47c753e7d40554346bc64
isailaandrei <isailaandrei.i@gmail.com>
2013-12-28 19:41:59 +0200

    aproape merge

---
+++ ex2/script.js
@@ -0,0 +1,10 @@
+window.onload=function() {
+function peste()
+{
+	document.getElementsByTagName("button")[0].onclick=function()
+	{
+		document.getElementById("target").style.display="block";
+	};
+};
+}
+
