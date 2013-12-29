33075daf10e0357cf5d778eac59e66fdedf80814
isailaandrei <isailaandrei.i@gmail.com>
2013-12-29 21:32:55 +0200

    nici asa..

--- ex2/script.js
+++ ex2/script.js
@@ -1,21 +1,26 @@
-function peste()	
-{
+
+
+function peste(){	
 	if (document.getElementById("target").style.display=="none")
+		{ document.getElementsByTagName("button")[0].onclick=function()
+	{
+		document.getElementById("target").style.display="block"
+	}
+};};
+window.onload=function() {peste()};
+function peste2()	
+{
+	if (document.getElementById("target").style.display=="block")
 	{
 	document.getElementsByTagName("button")[0].onclick=function()
 	{
-		document.getElementById("target").style.display="block"
-	}};
-	if (document.getElementById("target").style.display=="block")
-		{ document.getElementsByTagName("button")[0].onclick=function()
-	{
 		document.getElementById("target").style.display="none"
-	}
-};
+	}};};
 	
 
-};
-window.onload=function() { peste()};
+
+
+
 
 
 
