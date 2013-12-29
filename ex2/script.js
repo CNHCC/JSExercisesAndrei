32b31150c29faa5f39902cf196a2e513391685f1
isailaandrei <isailaandrei.i@gmail.com>
2013-12-29 17:08:22 +0200

    aproapee

--- ex2/script.js
+++ ex2/script.js
@@ -1,13 +1,22 @@
-function peste()	{
+function peste()	
+{
+	if (document.getElementById("target").style.display=="none")
+	{
 	document.getElementsByTagName("button")[0].onclick=function()
 	{
 		document.getElementById("target").style.display="block"
-	};
+	}};
+	if (document.getElementById("target").style.display=="block")
+		{ document.getElementsByTagName("button")[0].onclick=function()
+	{
+		document.getElementById("target").style.display="none"
+	}
+};
 	
 
 };
-
 window.onload=function() { peste()};
+
 
 
 
